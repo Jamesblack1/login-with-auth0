0bdc102f17f05941767e2ce09fef66625232af15
Jaime De la Fuente <34488833+Jamesblack1@users.noreply.github.com>
2022-02-24 10:36:56 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-8wrskji0.us.auth0.com',
+      clientId: 'E1Bfb7mgUvk8Ql6MkeHz4dAxWOMvX5cF'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
